7d131501d786e2d9aaf0280710e634f7b244629d
Ben Jordan <ben@poecoh.com>
2021-08-17 13:59:20 -0400

    Create GitUpdateExisting.ps1

---
+++ ScriptBlocks/GitUpdateExisting.ps1
@@ -0,0 +1,16 @@
+# Updates an existing file on github
+Param (
+    [System.String]$Uri
+    [System.String]$Headers
+    [System.String]$Message
+    [System.String]$Content
+)
+$Content = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($Content))
+$Sha = (Invoke-RestMethod -Uri $Uri -Method Get -Headers $Headers).sha
+$Body = @{message = $Message; content = $Content; sha = $Sha} | ConvertTo-Json
+$Response = Invoke-RestMethod -Uri $Uri -Method Put -Headers $Headers -Body $Body
+
+<#
+PS> & $ScriptBlock $Uri $Headers $Message $Content
+PS> Invoke-Command -ScriptBlock $ScriptBlock -ArgumentList $Uri,$Headers,$Message,$Content
+#>
