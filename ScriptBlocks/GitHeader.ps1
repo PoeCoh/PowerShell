589958b02cd6fcfae55294defe0febe4eaf65376
Ben Jordan <ben@poecoh.com>
2021-08-17 13:53:29 -0400

    Update GitHeader.ps1

--- ScriptBlocks/GitHeader.ps1
+++ ScriptBlocks/GitHeader.ps1
@@ -4,3 +4,13 @@ Param (
     [System.String]$Token
 )
 @{Authorization = "Basic $([System.Convert]::ToBase64String([System.Char[]]"$User`:$Token"))"}
+
+<#
+PS> # Get scriptblock from github
+PS> $Text = Invoke-RestMethod -Uri https://powershell.poecoh.com/ScriptBlocks/GitHeader.ps1
+PS> $ScriptBlock = [System.Management.Automation.ScriptBlock]::Create($Text)
+
+PS> # Invoke scriptblock
+PS> $Body = & $ScriptBlock $User $Token
+PS> $Body = Invoke-Command -ScriptBlock $ScriptBlock -ArgumentList $User,$Token
+#>
