69339779ae47f8b149674ace645cab4b9e085bc1
Ben Jordan <ben@poecoh.com>
2021-08-17 13:18:14 -0400

    Create GitBody.ps1

---
+++ ScriptBlocks/GitBody.ps1
@@ -0,0 +1,8 @@
+# Creates a body for a git put request
+Param (
+    [System.String]$Message,
+    [System.String]$Content,
+    [System.String]$Sha
+)
+$Content = [System.Convert]::ToBase64String([System.Text.Encoding]::ASCII.GetBytes($Content))
+@{message = $Message; content = $Content; sha = $Sha} | ConvertTo-Json
