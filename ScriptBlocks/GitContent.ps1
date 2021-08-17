3009b0703df12d676a61ea48b656bbdb38c61684
Ben Jordan <ben@poecoh.com>
2021-08-17 13:31:43 -0400

    Update and rename GitContent.ps1 to GitContentExtract.ps1

--- ScriptBlocks/GitContent.ps1
+++
@@ -1,5 +0,0 @@
-# Extracts content from git response (base64 to string)
-Param (
-    [System.String]$Content
-)
-[System.Text.Encoding]::ASCII.GetString([System.Convert]::FromBase64String($Content))
