7350a87ae6f2e1c2358c79d7e203e7c656e5cdd9
Ben Jordan <ben@poecoh.com>
2021-08-17 14:00:11 -0400

    Update GitContentExtract.ps1

--- ScriptBlocks/GitContentExtract.ps1
+++ ScriptBlocks/GitContentExtract.ps1
@@ -7,11 +7,6 @@ Param (
 # You could get content in plain text if you used the raw download link supplied in the response.
 # However it's another call and we already have the encoded content, so just cut out another call and decode it.
 <#
-PS> # Get scriptblock from github
-PS> $Text = Invoke-RestMethod -Uri https://powershell.poecoh.com/ScriptBlocks/GitContentExtract.ps1
-PS> $ScriptBlock = [System.Management.Automation.ScriptBlock]::Create($Text)
-
-PS> # Invoke scriptblock
 PS> $Body = & $ScriptBlock $Content
 PS> $Body = Invoke-Command -ScriptBlock $ScriptBlock -ArgumentList $Content
 #>
